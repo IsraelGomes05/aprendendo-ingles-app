76d1c80e9e3755b35914aca31582c2168226efe2
IsraelGomes05 <israelgomes05@gmail.com>
2018-10-27 00:17:53 -0400

    Adicionado tentativas e frases

---
+++ src/app/painel/frases-mock.ts
@@ -0,0 +1,8 @@
+import {Frase} from '../shared/frase.model';
+
+export const FRASES: Frase[] = [
+  {fraseEng: 'I like to learn', frasePtr: 'Eu gosto de aprender'},
+  {fraseEng: 'I watch tv', frasePtr: 'Eu assisto tv'},
+  {fraseEng: 'how are you?', frasePtr: 'como vai você'},
+  {fraseEng: 'I eat bread', frasePtr: 'Eu como pão'}
+];
