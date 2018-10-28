3488c8020accaecf2e7ad4871999d51a5e1950a4
IsraelGomes05 <israelgomes05@gmail.com>
2018-10-27 20:58:07 -0400

    Capturando as tentativas do painel e melhorias no painel

--- src/app/tentativas/tentativas.component.ts
+++ src/app/tentativas/tentativas.component.ts
@@ -1,18 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Coracao} from '../shared/coracao.model';
 
 @Component({
   selector: 'app-tentativas',
   templateUrl: './tentativas.component.html',
   styleUrls: ['./tentativas.component.css']
 })
-export class TentativasComponent implements OnInit {
+export class TentativasComponent implements OnInit, OnChanges {
 
-  public coracaoVazio = '/assets/coracao_vazio.png';
-  public coracaoCheio = '/assets/coracao_cheio.png';
+  @Input() public tentativas: number;
 
-  constructor() { }
+  public coracoes: Coracao[] = [
+    new Coracao(true),
+    new Coracao(true),
+    new Coracao(true)
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(): void {
+
   }
 
 }
